11d350eebc040f5cba8b0f5f5c6e8a6af96d3b45
Tong Zou <tzou@splunk.com>
2020-07-20 23:02:15 -0700

    fixed some bugs

--- tong/frozen_lake_mc.py
+++ tong/frozen_lake_mc.py
@@ -26,12 +26,27 @@ def get_policy(Q = None, epsilon_soft = True, epsilon = 0.1):
     else:
         policy = [0 for i in range(env.nS)]
         for state in range(env.nS):
-            # q = np.array(Q[state])
-            # max = q.max()
-            # max_index = np.where(q == max)[0]
-            # policy[state] = [1/len(max_index) if i in max_index else 0 for i in range(env.nA)]
-            policy[state] = np.argmax(Q[state])
+            q = np.array(Q[state])
+            max = q.max()
+            max_index = np.where(q == max)[0]
+            policy[state] = [1/len(max_index) if i in max_index else 0 for i in range(env.nA)]
+            # policy[state] = np.argmax(Q[state])
     return policy
+
+def print_policy(policy):
+    for s in range(0, 16, 4):
+        text = ''
+        for t in range(s, s+4):
+            v = np.argmax(policy[t]) 
+            if v == 2:
+                text += '> '
+            if v == 0:
+                text += '< '
+            if v == 1:
+                text += 'v '
+            if v == 3:
+                text += '> '
+        print(text)
 
 def argmax_rand(arr):
     return np.random.choice(np.flatnonzero(arr == np.max(arr)))
@@ -64,22 +79,23 @@ def run_episode(policy, render = False):
     return states, rewards, actions, win
 
 def on_policy_improvement(episodes = 1000, first_visit = True, gamma = 0.9, epsilon = 0.1):
-    ## initialize random policy
-    policy = get_policy()
+    ## initialize random policy    
     Q = np.random.random((env.nS, env.nA))
+    policy = get_policy(Q, True, epsilon)
     returns = [[{ 'sum': 0, 'n': 0 } for j in range(env.nA)] for i in range(env.nS)]
 
     for e in range(episodes):
         states, rewards, actions, _ = run_episode(policy)
         G = 0
         visited = {}
-        for t in range(len(states)): # initialze visited
-            state = states[t]
-            reward = rewards[t]
-            action = actions[t]
-            key = str(state) + '-' + str(action)
-            if (not key in visited):
-                visited[key] = t
+        if (first_visit):
+            for t in range(len(states)): # initialze visited
+                state = states[t]
+                reward = rewards[t]
+                action = actions[t]
+                key = str(state) + '-' + str(action)
+                if (not key in visited):
+                    visited[key] = t
 
         for t in range(len(states) - 1, -1, -1):
             state = states[t]
@@ -87,53 +103,40 @@ def on_policy_improvement(episodes = 1000, first_visit = True, gamma = 0.9, epsi
             action = actions[t]
             key = str(state) + '-' + str(action)
 
-            G = gamma*G + reward
+            G = gamma * G + reward
             if (not first_visit or visited[key] == t):
                 returns[state][action]['sum'] += G
                 returns[state][action]['n'] += 1
-                Q[state, action] = 0 if returns[state][action]['n'] == 0 else returns[state][action]['sum'] / returns[state][action]['n']
-                A = np.argmax(Q[state])
-                for a in range(env.nA):
-                    if a == A:
-                        policy[state][a] = 1 - epsilon + epsilon/env.nA
-                    else:
-                        policy[state][a] = epsilon/env.nA
+                Q[state, action] = returns[state][action]['sum'] / returns[state][action]['n']
+                policy = get_policy(Q, True, epsilon)
+    print(returns)
     return Q
 
-def off_policy_improvement(episodes = 1000, first_visit = True, gamma = 0.9, epsilon = 0.1):
+def off_policy_improvement(episodes = 1000, gamma = 0.9, epsilon = 0.1):
     ## initialize random policy
     bpolicy = get_policy()
-    Q = np.random.random((env.nS, env.nA))
+    Q = np.zeros((env.nS, env.nA))
     C = [[0 for j in range(env.nA)] for i in range(env.nS)]
-    returns = [[{ 'sum': 0, 'n': 0 } for j in range(env.nA)] for i in range(env.nS)]
-    tpolicy = get_policy()
+    tpolicy = get_policy(Q)
 
     for e in range(episodes):
         states, rewards, actions, _ = run_episode(bpolicy)
         G = 0
         W = 1
-        # visited = {}
-        # for t in range(len(states)): # initialze visited
-        #     state = states[t]
-        #     reward = rewards[t]
-        #     action = actions[t]
-        #     key = str(state) + '-' + str(action)
-        #     if (not key in visited):
-        #         visited[key] = t
 
         for t in range(len(states) - 1, -1, -1):
             state = states[t]
             reward = rewards[t]
             action = actions[t]
 
-            G = gamma*G + reward
+            G = gamma * G + reward
             C[state][action] += W
             Q[state][action] += (W/C[state][action]) * (G - Q[state][action])
-            tpolicy[state] = argmax(Q[state])
+            # tpolicy[state] = argmax(Q[state])
+            tpolicy = get_policy(Q)
             if (tpolicy[state][action] == 0):
                 break
-            W = W * env.nA
-                
+            W = W / bpolicy[state][action]
     return Q
 
 
@@ -156,13 +159,14 @@ def get_score(env, policy, episodes=1000):
                 misses += 1
                 break
     print('----------------------------------------------')
-    print('You took an average of {:.0f} steps to get the frisbee'.format(np.mean(steps_list)))
+    if (misses != episodes):
+        print('You took an average of {:.0f} steps to get the frisbee'.format(np.mean(steps_list)))
     print('And you fell in the hole {:.2f} % of the times'.format((misses/episodes) * 100))
     print('----------------------------------------------')
 
-# Q = on_policy_improvement(30000)
-Q = off_policy_improvement(10000)
+Q = on_policy_improvement(10000, True, 0.99, 0.1)
+# Q = off_policy_improvement(20000, 0.99)
 print(Q)
 policy = get_policy(Q, False)
-print(policy)
+print_policy(policy)
 get_score(env, policy)
